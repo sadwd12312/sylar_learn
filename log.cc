127673c826071cbb3e8f2ca9fa1c46b34e645bd7
fys <965667773@qq.com>
2019-11-07 18:47:11 +0800

    up to 04-Appender

--- log.cc
+++ log.cc
@@ -1,1 +1,80 @@
-a
+#include"log.h"
+
+namespace syalr {
+	Logger::Logger(const std::string& name)
+		:m_name(name) {}
+
+
+	void Logger::addAppender(LogAppender::ptr appender);
+	{
+		m_appenders.push_back(appender);
+	}
+	void Logger::delAppender(LogAppender::ptr appender);
+	{
+		for (auto it = m_appenders.begin(); it!m_appenders.den(); ++it)
+			if (*it == appender)
+			{
+				m_appenders.erase(it);
+				break;
+			}
+	}
+	void Logger::log(LogLevel::Level level, LogEvent::ptr event)
+	{
+		if (level >= m_level)
+		{
+			for (auto& i : m_appenders)
+			{
+				i->log(level, event);
+			}
+		}
+	}
+
+	void Logger::debug(LogEvent::ptr event);
+	{
+		debug(LogLevel::DEBUG, level);
+	}
+	void Logger::info(LogEvent::ptr event);
+	{
+		debug(LogLevel::INFO, level);
+	}
+	void Logger::warn(LogEvent::ptr event);
+	{
+		debug(LogLevel::WARN, level);
+	}
+	void Logger::error(LogEvent::ptr event);
+	{
+		debug(LogLevel::ERROR, level);
+	}
+	void Logger::fatal(LogEvent::ptr event);
+	{
+		debug(LogLevel::FATAL, level);
+	}
+
+	FileLogAppender::FileLogAppender(const std::string& filename)
+		:m_filename(filename) {}
+
+	void FileLogAppender::log(LogLevel::Level level, LogEvent::ptr event)
+	{
+		if (level >= m_level)
+		{
+			m_filestream << m_formatter.format(event);
+		}
+	}
+	
+	bool FileLogAppender::reopen()
+	{
+		if (m_filestream)
+		{
+			m_filestream.close();
+		}
+		m_filestream.open(m_filename);
+		return !!m_filestream;
+	}
+	void StdoutLogAppender::log(LogLevel::Level level, LogEvent::ptr event)
+	{
+		if (level >= m_level)
+		{
+			std::cout << m_formatter.format(event);
+		}
+	}
+}
--- log.h
+++ log.h
@@ -1,1 +1,151 @@
-s
+#ifndef _SYLAR_LOG_H_
+#define _SYLAR_LOG_H_
+
+#include<string>
+#include<stdint.h>
+#include<memory>
+#include<list>
+#include<stringstream>
+#include<fstream>
+#include<vector>
+
+//日志事件
+class LogEvent {
+public:
+	typedef std::shared_ptr<LogEvent> ptr;
+	LogEvent();
+private:
+	const char* m_file = nullptr; //文件名
+	int32_t m_line = 0;//行号
+	uint32_t m_elapse = 0;//程序启动到现在的毫秒数
+	uint32_t m_threadId = 0;//线程id
+	uint32_t m_fiberId = 0;//协程id
+	uint64_t m_time = 0;//时间戳
+	std::string m_content;
+};
+//日志级别
+class LogLevel {
+public:
+	enum Level
+	{
+		DEBUG = 1,
+		INFO=2,
+		WARN=3,
+		ERROR=4,
+		FATAL=5
+	};
+};
+
+//日志格式器
+class LogFormatter
+{
+public:
+	typedef std::shared_ptr<LogFormatter> ptr;
+	LogFormatter(const std::string& pattern);
+
+
+	std::string format(LogEvent::ptr event);
+private:
+	class FormatItem
+	{
+	public:
+		typedef std::shared_ptr<FormatItem> ptr;
+		virtual ~FormatItem(){}
+		virtual std::string format(LogEvent::ptr event) = 0;
+	};
+	void init();
+private:
+	std::string m_pattern;
+	std::vector<FormatItem::ptr> m_items;
+};
+
+//日志输出地
+class LogAppender
+{
+public:
+	typedef std::shared_ptr<LogAppender> ptr;
+	virtual ~LogAppender() {}
+
+	virtual void log(LogLevel::Level level, LogEvent::ptr event) = 0;
+
+	void setFormatter(LogFormatter::ptr val) { m_formatter = val; }
+	LogFormatter::ptr getFormatter()const { return m_formatter; }
+protected:
+	LogLevel::Level m_level;
+	LogFormatter::ptr m_formatter;
+};
+
+//日志器
+class Logger {
+public:
+	typedef std::shared_ptr<Logger> ptr;
+
+	Logger(const std::string& name = "root");
+	void log(LogLevel::Level level, LogEvent::ptr event);
+
+	void debug(LogEvent::ptr event);
+	void info(LogEvent::ptr event);
+	void warn(LogEvent::ptr event);
+	void error(LogEvent::ptr event);
+	void fatal(LogEvent::ptr event);
+
+	void addAppender(LogAppender::ptr appender);
+	void delAppender(LogAppender::ptr appender);
+	LogLevel::Level getLevel()const { return m_level; }
+	void setLevel(LogLevel::Level val) { m_level = val; }
+private:
+	std::string m_name;//日志名称
+	LogLevel::Level m_level;//日志级别
+	std::list<LogAppender::ptr> m_appenders;
+};
+
+//输出到控制台的Appender
+class StdoutLogAppender :public LogAppender {
+public:
+	typedef std::shared_ptr<stdoutLogAppender> ptr;
+	void log(LogLevel::Level, LogEvent::ptr event)override;
+};
+//输出到文件的Appender
+class FileLogAppender :public LogAppender{
+public:
+	typedef std::shared_ptr<fileLogAppender> ptr;
+	FileLogAppender(const std::string& filename);
+	void log(LogLevel::Level, LogEvent::ptr event)override;
+	//重新打开文件,文件打开成功,返回true
+	bool reopen();
+private:
+	std::string m_filename;
+	std::ofstrram m_filestream;
+}
+//#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
